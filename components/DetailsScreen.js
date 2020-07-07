561cd928979a08189c3aa65f66ace6746117ffa4
Alli Colyer <allicolyer@gmail.com>
2020-07-06 22:50:10 -0700

    replace styles in components with shared styles

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { theme } from "../utils/theme";
-import { StyleSheet, TouchableOpacity, View, Text, Image } from "react-native";
+import styles from "../utils/appStyles";
+import { TouchableOpacity, View, Text, Image } from "react-native";
 
 const DetailsScreen = ({ route, navigation }) => {
   const { image } = route.params;
   return (
-    <View>
+    <View style={[styles.flexColumn, styles.flexOne, styles.margin]}>
       <Image
         resizeMode="stretch"
         key={`id${image.id}`}
         source={{ uri: image.webformatURL }}
-        style={styles.image}
+        style={[
+          styles.roundedBorder,
+          styles.marginHorizontal,
+          styles.marginVertical,
+          styles.largeImage,
+        ]}
       />
-      <Text>Uploaded By: {image.user}</Text>
-      <Text>Tags: {image.tags}</Text>
-      <Text>
+      <Text style={styles.marginHorizontal}>Uploaded By: {image.user}</Text>
+      <Text style={styles.marginHorizontal}>Tags: {image.tags}</Text>
+      <Text style={styles.marginHorizontal}>
         Resolution: {image.imageWidth} x {image.imageHeight}
       </Text>
       <TouchableOpacity
-        style={styles.button}
+        style={(styles.marginHorizontal, styles.button)}
         onPress={() => navigation.goBack()}
       >
         <Text style={styles.buttonText}>Go Back</Text>
@@ -26,26 +31,5 @@ const DetailsScreen = ({ route, navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  button: {
-    width: 100,
-    backgroundColor: theme.colors.primary,
-    padding: theme.space[2],
-    borderRadius: theme.radii[0],
-    marginHorizontal: theme.space[1],
-  },
-  buttonText: {
-    color: theme.colors.white,
-    textAlign: "center",
-  },
-  image: {
-    width: 300,
-    height: 300,
-    marginVertical: theme.space[1],
-    marginHorizontal: theme.space[1],
-    borderRadius: theme.radii[0],
-  },
-});
 
 export default DetailsScreen;
