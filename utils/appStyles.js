7ed49b001880f1d0508699dbfa6caa7052722269
Alli Colyer <allicolyer@gmail.com>
2020-07-06 22:29:22 -0700

    add shared stylesheet

---
+++ utils/appStyles.js
@@ -0,0 +1,76 @@
+import { StyleSheet } from "react-native";
+import { theme } from "./theme";
+
+const utilites = {
+  flexColumn: {
+    flexDirection: "column",
+    alignSelf: "stretch",
+    alignItems: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flexOne: {
+    flex: 1,
+  },
+  marginHorizontal: {
+    marginHorizontal: theme.space[1],
+  },
+  marginVertical: {
+    marginVertical: theme.space[1],
+  },
+  margin: {
+    margin: theme.space[1],
+  },
+  roundedBorder: {
+    borderRadius: theme.radii[0],
+  },
+};
+
+const buttons = {
+  button: {
+    width: 100,
+    backgroundColor: theme.colors.primary,
+    padding: theme.space[2],
+    borderRadius: theme.radii[0],
+  },
+  buttonText: {
+    color: theme.colors.white,
+    textAlign: "center",
+  },
+};
+
+const text = {
+  centerText: { textAlign: "center" },
+  headerText: {
+    marginVertical: theme.space[2],
+    color: theme.colors.primary,
+    fontSize: theme.fontSizes[3],
+  },
+  primaryText: {
+    color: theme.colors.primary,
+  },
+};
+
+const images = {
+  smallImage: {
+    width: 100,
+    height: 100,
+  },
+  largeImage: {
+    width: 300,
+    height: 300,
+  },
+};
+
+const styles = StyleSheet.create({
+  ...utilites,
+  ...buttons,
+  ...text,
+  ...images,
+});
+
+export default styles;
