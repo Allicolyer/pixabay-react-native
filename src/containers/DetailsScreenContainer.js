a1fe696b2dac8aff272dead000e89b26ac3d916f
Alli Colyer <allicolyer@gmail.com>
2020-07-07 15:24:27 -0700

    rearrange files and create src folder

--- src/containers/DetailsScreenContainer.js
+++ src/containers/DetailsScreenContainer.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import DetailsScreen from "../components/DetailsScreen";
+import DetailsScreen from "../screens/DetailsScreen";
 
 //maps the screen orientation from the redux store to the DetailsScreen props
 const mapStateToProps = (state) => {
