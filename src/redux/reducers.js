0b03afdd281d0840d8133ba01d1725f864faff17
Alli Colyer <allicolyer@gmail.com>
2020-07-07 23:00:04 -0700

    calculate initial screen dimensions

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,6 +4,8 @@ import {
   UPDATE_SEARCH_RESULTS,
   CHANGE_SCREEN_DIMENSIONS,
 } from "./actions";
+import { Dimensions } from "react-native";
+import { calculateScreenOrientation } from "../utils/helpers";
 
 //keeps track of the search results for the user's search input text
 const searchResults = (state = [], action) => {
@@ -25,15 +27,16 @@ const searchTextInput = (state = "", action) => {
   }
 };
 
+//figure out initial screenDimensions
+const window = Dimensions.get("window");
+const initialScreenDimensions = {
+  screenWidth: window.width,
+  screenHeight: window.height,
+  screenOrientation: calculateScreenOrientation(window.width, window.height),
+};
+
 //changes to the screen orientation are listened for in ./components/Content
-const screenDimensions = (
-  state = {
-    screenWidth: 100,
-    screenHeight: 100,
-    screenOrientation: "portrait",
-  },
-  action
-) => {
+const screenDimensions = (state = initialScreenDimensions, action) => {
   switch (action.type) {
     case CHANGE_SCREEN_DIMENSIONS:
       return {
