1f2f48d8113b1a402f23b15614934c77b6fce638
Alli Colyer <allicolyer@gmail.com>
2020-07-07 18:20:58 -0700

    adjust number of columns based on image and screen width

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,21 +1,25 @@
 import React from "react";
 import styles from "../style/appStyles";
+import { calculateImageListColumns } from "../utils/helpers";
 import { TouchableOpacity, FlatList, Image, View, Text } from "react-native";
 
 // The ImageList Component displays all images from a user's search
 const ImageList = ({ navigation, results, screenOrientation }) => {
   //results need to be parsed before they can be rendered in the FlatList component
   const parsed = JSON.parse(results);
+  //calculate the number of columns
+  const numofColumns = calculateImageListColumns(16);
 
   return (
-    <View style={[styles.margin, styles.flexOne]}>
+    <View style={[styles.flexOne]}>
       {/* if there are any search results render the Flatlist */}
       {parsed.length ? (
         <FlatList
+          //this rerenders the flatList every time there is a change in the screen orientation
           key={screenOrientation}
           data={parsed}
           // the number of columns changes based on the screen orientation
-          numColumns={screenOrientation === "portrait" ? 3 : 5}
+          numColumns={numofColumns}
           renderItem={({ item }) => (
             <TouchableOpacity
               onPress={() => {
@@ -29,7 +33,7 @@ const ImageList = ({ navigation, results, screenOrientation }) => {
                 resizeMode="cover"
                 key={`id${item.id}`}
                 source={{ uri: item.previewURL }}
-                style={[styles.roundedBorder, styles.margin, styles.smallImage]}
+                style={[styles.roundedBorder, styles.imageThumbnail]}
               />
             </TouchableOpacity>
           )}
