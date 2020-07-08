ffe843e4883bed6634801256f1ca3d1487119323
Alli Colyer <allicolyer@gmail.com>
2020-07-08 10:24:09 -0700

    remove store dependencies in the helper functions to prevent require cycles

--- src/containers/DetailsScreenContainer.js
+++ src/containers/DetailsScreenContainer.js
@@ -4,7 +4,7 @@ import DetailsScreen from "../screens/DetailsScreen";
 //maps the screen orientation from the redux store to the DetailsScreen props
 const mapStateToProps = (state) => {
   return {
-    screenOrientation: state.screenDimensions.screenOrientation,
+    screenDimensions: state.screenDimensions,
   };
 };
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,12 +1,12 @@
 import { env } from "../../.env";
-import store from "../redux/store";
 import styles from "../style/appStyles";
+import { Dimensions } from "react-native";
 
 // API call to pixabay
-export const pixabayAPICall = async () => {
+export const pixabayAPICall = async (text) => {
   // encode the input text from the redux store
-  if (store.getState().searchTextInput.length) {
-    const q = encodeURIComponent(store.getState().searchTextInput);
+  if (text.length) {
+    const q = encodeURIComponent(text);
     //fetch the URL with the encoded query
     return fetch(
       `https://pixabay.com/api/?key=${env.PIXABAY_KEY}&q=${q}&image_type=photo&pretty=true`
@@ -31,13 +31,23 @@ export const calculateScreenOrientation = (width, height) => {
   }
 };
 
+//figure out initial screenDimensions based on the window size
+export const calculateInitalScreenDimensions = () => {
+  const window = Dimensions.get("window");
+  return {
+    screenWidth: window.width,
+    screenHeight: window.height,
+    screenOrientation: calculateScreenOrientation(window.width, window.height),
+  };
+};
+
 //calculate the image display dimensions for the detail screen
-export const calculateImageDisplayDimensions = (inputWidth, inputHeight) => {
-  const {
-    screenWidth,
-    screenHeight,
-    screenOrientation,
-  } = store.getState().screenDimensions;
+export const calculateImageDisplayDimensions = (
+  screenDimensions,
+  inputWidth,
+  inputHeight
+) => {
+  const { screenWidth, screenHeight, screenOrientation } = screenDimensions;
 
   let imageWidth, imageHeight;
   let imageRatio = inputWidth / inputHeight;
@@ -47,14 +57,14 @@ export const calculateImageDisplayDimensions = (inputWidth, inputHeight) => {
     imageWidth = 0.85 * screenWidth;
     //scale the height in proportion
     imageHeight = (1 / imageRatio) * imageWidth;
-    //for extra tall images, make sure the image is not too tall for the screen. If it is, rescale according to the screen width.
+    //for extra tall images, make sure the image is not too tall for the screen. If it is, rescale according to the screen height.
     if (imageHeight > 0.6 * screenHeight) {
       imageHeight = 0.6 * screenHeight;
       imageWidth = imageRatio * imageHeight;
     }
   } else {
-    //in landscape mode, the image height should be 60% of screen height
-    imageHeight = 0.8 * screenHeight;
+    //in landscape mode, the image height should be 70% of screen height
+    imageHeight = 0.7 * screenHeight;
     //scale the width in proportion
     imageWidth = imageRatio * imageHeight;
     //for extra wide images, make sure the image is still not too wide for the screen. If it is, rescale according to the screen width.
@@ -68,12 +78,11 @@ export const calculateImageDisplayDimensions = (inputWidth, inputHeight) => {
 };
 
 //calculate the number of columns that should be shown in the image list
-export const calculateImageListColumns = (marginInPixels) => {
+export const calculateImageListColumns = (screenWidth, marginInPixels) => {
   //calcuate imageWidth using stylesheet
   imageWidth = styles.imageThumbnail.width + styles.imageThumbnail.margin;
   //get the width of the screen from the redux store, subtrack the width of the side margin
-  const availableWidth =
-    store.getState().screenDimensions.screenWidth - 2 * marginInPixels;
+  const availableWidth = screenWidth - 2 * marginInPixels;
   //return how many columns can fit
   return Math.floor(availableWidth / imageWidth);
 };
