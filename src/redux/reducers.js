cba40b0b00fd2317444bc3f88815d07271382b57
Alli Colyer <allicolyer@gmail.com>
2020-07-08 10:24:09 -0700

    remove store dependencies in the helper functions to prevent require cycles

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,8 +4,7 @@ import {
   UPDATE_SEARCH_RESULTS,
   CHANGE_SCREEN_DIMENSIONS,
 } from "./actions";
-import { Dimensions } from "react-native";
-import { calculateScreenOrientation } from "../utils/helpers";
+import { calculateInitalScreenDimensions } from "../utils/helpers";
 
 //keeps track of the search results for the user's search input text
 const searchResults = (state = null, action) => {
@@ -27,16 +26,11 @@ const searchTextInput = (state = "", action) => {
   }
 };
 
-//figure out initial screenDimensions
-const window = Dimensions.get("window");
-const initialScreenDimensions = {
-  screenWidth: window.width,
-  screenHeight: window.height,
-  screenOrientation: calculateScreenOrientation(window.width, window.height),
-};
-
 //changes to the screen orientation are listened for in ./components/Content
-const screenDimensions = (state = initialScreenDimensions, action) => {
+const screenDimensions = (
+  state = calculateInitalScreenDimensions(),
+  action
+) => {
   switch (action.type) {
     case CHANGE_SCREEN_DIMENSIONS:
       return {
