16309c26488c18d6a7bffaee999d8fbb2d96ee14
Alli Colyer <allicolyer@gmail.com>
2020-07-09 09:56:53 -0700

    add prop types to components

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../style/appStyles";
 import ImageList from "./ImageList";
 import { View, Text } from "react-native";
+import PropTypes from "prop-types";
 
 // The SearchResults Component displays either an ImageList or text showing no results
 const SearchResults = ({ navigation, results, totalHits, screenWidth }) => {
@@ -23,6 +24,13 @@ const SearchResults = ({ navigation, results, totalHits, screenWidth }) => {
       )}
     </View>
   );
+};
+
+SearchResults.propTypes = {
+  navigation: PropTypes.object.isRequired,
+  results: PropTypes.string,
+  totalHits: PropTypes.number,
+  screenWidth: PropTypes.number.isRequired,
 };
 
 export default SearchResults;
