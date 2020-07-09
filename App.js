3582a3bd1bfdb398aef003ab6e2dbff2fb59f6bb
Alli Colyer <allicolyer@gmail.com>
2020-07-09 10:35:13 -0700

    move expo status bar to main app component

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 import Content from "./src/components/Content";
+import { StatusBar } from "expo-status-bar";
 
 export default App = () => {
   return (
     <Provider store={store}>
       <Content />
+      <StatusBar style="auto" />
     </Provider>
   );
 };
